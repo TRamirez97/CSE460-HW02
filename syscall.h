ddbc22b253f9ab50e59a39596108f80cdcf033b2
TRamirez97 <timothy.ramirez1992@gmail.com>
2020-03-09 14:46:34 -0700

    Added all system calls needed into syscall.h

--- syscall.h
+++ syscall.h
@@ -20,3 +20,14 @@
 #define SYS_link   19
 #define SYS_mkdir  20
 #define SYS_close  21
+#define SYS_date   22
+#define SYS_getuid 23
+#define SYS_getgid 24
+#define SYS_getppid 25
+#define SYS_setuid 26
+#define SYS_setgid 27
+#define SYS_cps	   28
+#define SYS_nps    29
+#define SYS_chmod  30
+#define SYS_chown  31
+#define SYS_chgrp  32
